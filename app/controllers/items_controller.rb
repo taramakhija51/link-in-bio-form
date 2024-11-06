e0b19f7a8b9472134bdd9a048c6df4b5e09d31e0
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-11-06 00:03:26 +0000

    note: does not work unless https is in link

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,15 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+  def backdoor_form
+    render({ :template => "item_templates/backdoor" })
+  end
+  def backdoor_results
+    new_item = Item.new
+    new_item.link_url = params.fetch("link_url")
+    new_item.link_description = params.fetch("link_description")
+    new_item.thumbnail_url = params.fetch("thumb_url")
+    new_item.save
+    redirect_to("/", allow_other_host: true)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
+  get("/backdoor", { :controller => "items", :action => "backdoor_form" })
+  post("/backdoor_form", { :controller => "items", :action => "backdoor_results" })
 end
